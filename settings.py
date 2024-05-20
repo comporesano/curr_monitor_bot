6a0e1554b6869105103e2b3163a37bc5397258d7
Roman <r_bakharev@mail.ru>
2024-05-20 15:44:41 +0300

    Functional complete

---
+++ settings.py
@@ -0,0 +1,17 @@
+# API KEY
+API_KEY = 'api-key'
+
+# BOT TOKEN
+TOKEN = 'token'
+
+# Currencies list API
+CL_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+# Price currency API
+PC_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+# SAVING PATH
+PATH = './monitoring.json'
+
+# GROUP CHAT ID
+CHAT_ID = 'chat_id'
